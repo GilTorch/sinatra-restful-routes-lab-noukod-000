b71be420e66f2f7e55722fa531d2c2f9e1fa055b
Gilbert Torchon <gilbert@noukod.com>
2019-01-12 18:47:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,35 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
     erb :index
   end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe=Post.create(name:@params[:name],ingredients:@params[:ingredients],cook_time:@params[:cook_time])
+    redirect('/recipes/#{@id}')
+  end
+
+  get '/recipes' do
+    @recipes=Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    erb :edit
+  end
+
+  get '/recipes' do
+
+  end
+
 end
